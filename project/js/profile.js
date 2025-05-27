674e3bd09f21ec0617507245a227e4143819814e
dreme154 <sandyevans617@gmail.com>
2025-05-27 20:22:04 +0100

    Update profile.js

--- project/js/profile.js
+++ project/js/profile.js
@@ -364,13 +364,6 @@ const celebs = [
 		profile: "Lilith Cavaliere is an Australian social media personality and influencer, known for her fashion and lifestyle content, particularly on Instagram and YouTube."
 	},
 	{
-		name: 'Chase Matthew',
-		img: 'images/Chase-Matthew.png',
-		profession: 'Country music Musician,Song Writer',
-		age: '27 (born December 20, 1997)',
-		profile: "Chase Matthew McQuitty is an American country music singer and songwriter. Signed to Warner Records Nashville, he charted in late 2023 with the single Love You Ag by ain"
-	},
-	{
 		name: 'Alanis Nadine Morissette',
 		img: 'images/alanis_nadine.jpg',
 		profession: 'singer, songwriter and actress',
